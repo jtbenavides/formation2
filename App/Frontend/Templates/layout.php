d6157d441c7a34b445e103c72f645b884b1a88f5
John BENAVIDES <jbenavides@DREAMCENTURY.local>
2016-05-04 09:46:38 +0200

    Base + Mobile_Detect

---
+++ App/Frontend/Templates/layout.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>
+      <?= isset($title) ? $title : 'Mon super site' ?>
+    </title>
+ 
+    <meta charset="utf-8" />
+ 
+    <link rel="stylesheet" href="/css/Envision.css" type="text/css" />
+  </head>
+ 
+  <body>
+    <div id="wrap">
+      <header>
+        <h1><a href="/">Mon super site</a></h1>
+        <p>Comment ça, il n'y a presque rien ?</p>
+      </header>
+ 
+      <nav>
+        <ul>
+            <li><a href="/home">Accueil</a></li>
+            <?php if ($user->isAuthenticated()) { ?>
+                <li><a href="/admin/">Admin</a></li>
+                <li><a href="/admin/news-insert.html">Ajouter une news</a></li>
+                <li><a href="/admin/logout">Deconnection</a></li>
+          <?php }else{ ?>
+                <li><a href="/admin/">Connection</a></li>
+          <?php } ?>
+		  <?php require_once '../vendor/mobiledetect/mobiledetectlib/Mobile_Detect.php';
+			$detect = new Mobile_Detect;
+			if (!$detect->isMobile() && !$detect->isTablet()){ ?>
+				<li><a href="/admin/">Connection</a></li>
+			<?php } ?>
+        </ul>
+      </nav>
+ 
+      <div id="content-wrap">
+        <section id="main">
+          <?php if ($user->hasFlash()) echo '<p style="text-align: center;">', $user->getFlash(), '</p>'; ?>
+ 
+          <?= $content ?>
+        </section>
+      </div>
+ 
+      <footer></footer>
+    </div>
+  </body>
+</html>
